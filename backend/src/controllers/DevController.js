f015d21b8a4bd2a4e3dc0517f3855c721836b33b
Rogério Nascimento <rogerio.nascimento@soterotech.com.br>
2020-01-15 09:44:00 -0200

    implementa cadastro do dev

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,65 +2,31 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const Dev = require('../models/Dev');
 
-class DevController{
+
+module.exports = {
 
   async store(req,res){
-    const {github_username, techs} = req.body;
+    const {github_username, techs, latitude, longitude} = req.body;
 
-     const teste = await Dev.find({github_username:github_username}).then((resp) => {
-      if(resp){
-        res.json(resp);
-        }
-     });
+      let dev = await Dev.findOne({github_username});
 
-
-    try {
+      if(!dev){
       const resApi = await axios.get(`https://api.github.com/users/${github_username}`);
       const techsArray = techs.split(',').map(tech => tech.trim());
       const {name = login, avatar_url, bio} = resApi.data;
-
-
-      const dev_created = await Dev.create({
+      const location ={
+        type:'Point',
+        coordinates: [longitude,latitude],
+      }
+      dev = await Dev.create({
              name,
-            github_username,
+             github_username,
              avatar_url,
              bio,
              techs:techsArray,
+             location,
            });
-
-       res.json(dev_created);
-
-
-    }catch(erro) {
-      console.log(erro);
-      // const {status,data} = erro.response;
-      // return res.status(status).json(data);
     }
-    // .then(function (response){
-    //   const {name = login, avatar_url, bio} = response.data;
-    //   const techsArray = techs.split(',').map(tech => tech.trim());
-
-
-    //   Dev.create({
-    //     name,
-    //     avatar_url,
-    //     bio,
-    //     techs:techsArray,
-    //   }).then(function (mongooseResp){
-    //     return res.json(mongooseResp);
-    //   });
-
-
-    // }).catch(function(erro){
-    //   const {status,data} = erro.response;
-    //   return res.status(status).json([]);
-    // });
-
-
-
-
+       res.json(dev);
   }
-
 }
-
-module.exports = new DevController();
