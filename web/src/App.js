84bc8d194f9294fdc6cb9a510cd2b9a0db348ce3
Rogerio Nascimento <rogerionascimento.dev@outlook.com.br>
2020-02-15 15:05:30 -0300

    finalizando layout de cadastro de dev e adicionando animaçoes no mesmo

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Global.css";
+import "./Main.css";
+import "./Aside.css";
+import "./App.css";
+import "./Animacoes.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário Github</label>
+            <input name="github_username" id="github_username" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="tecs">Tecnologias</label>
+            <input name="tecs" id="tecs" />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Latitude</label>
+              <input name="longitude" id="longitude" />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars1.githubusercontent.com/u/40906099"
+                alt="RogerioNascimento-dev"
+              />
+              <div className="user-info">
+                <strong>Rogério Nascimento</strong>
+                <span>ReactJS,React,NodeJS,PHP</span>
+              </div>
+            </header>
+            <p>
+              Analista de sistemas e desenvolvedor de softwares Web e Mobile
+            </p>
+            <a href="https://github.com/RogerioNascimento-dev">
+              Acessar perfil no Github
+            </a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars1.githubusercontent.com/u/40906099"
+                alt="RogerioNascimento-dev"
+              />
+              <div className="user-info">
+                <strong>Rogério Nascimento</strong>
+                <span>ReactJS,React,NodeJS,PHP</span>
+              </div>
+            </header>
+            <p>
+              Analista de sistemas e desenvolvedor de softwares Web e Mobile
+            </p>
+            <a href="https://github.com/RogerioNascimento-dev">
+              Acessar perfil no Github
+            </a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars1.githubusercontent.com/u/40906099"
+                alt="RogerioNascimento-dev"
+              />
+              <div className="user-info">
+                <strong>Rogério Nascimento</strong>
+                <span>ReactJS,React,NodeJS,PHP</span>
+              </div>
+            </header>
+            <p>
+              Analista de sistemas e desenvolvedor de softwares Web e Mobile
+            </p>
+            <a href="https://github.com/RogerioNascimento-dev">
+              Acessar perfil no Github
+            </a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars1.githubusercontent.com/u/40906099"
+                alt="RogerioNascimento-dev"
+              />
+              <div className="user-info">
+                <strong>Rogério Nascimento</strong>
+                <span>ReactJS,React,NodeJS,PHP</span>
+              </div>
+            </header>
+            <p>
+              Analista de sistemas e desenvolvedor de softwares Web e Mobile
+            </p>
+            <a href="https://github.com/RogerioNascimento-dev">
+              Acessar perfil no Github
+            </a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
